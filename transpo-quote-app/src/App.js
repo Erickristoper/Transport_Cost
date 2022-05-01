7e7980c46088b1920b18de67d5de55a0324457e0
Eric Del Soorro <erickdelsocorro@gmail.com>
2022-05-01 13:00:09 +0800

    😍 Firebase Hosting
    I've heard firebase several times in the past but it's confusing to me for the first time I saw it. Too many things. However, it is intuitive. You just need to focus on the things you need and move on.

--- transpo-quote-app/src/App.js
+++ transpo-quote-app/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import Quotation from './components/Quotation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
--- transpo-quote-app/src/index.js
+++ transpo-quote-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,3 +19,18 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAX9kAg_ax_ENavCEUDRfU4-G9WTKmVjL0",
+  authDomain: "transport-quotation.firebaseapp.com",
+  projectId: "transport-quotation",
+  storageBucket: "transport-quotation.appspot.com",
+  messagingSenderId: "624800374528",
+  appId: "1:624800374528:web:e0156e408f9d147efa9de5",
+  measurementId: "G-57CK97KHFB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
